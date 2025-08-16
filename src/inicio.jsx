578fe4077afb6068f791ca7f37ab5425ea2af969
KRAT00S1 <ra54029168884@acaonsfatima.org.br>
2025-08-16 00:47:04 -0300

    subi

--- src/inicio.jsx
+++ src/inicio.jsx
@@ -1,168 +1,152 @@
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import './inicio.scss';
 
-
-
-import './inicio.scss'
-
-
-
-import logo from './assets/images/logo.png'
-import curso from './assets/images/cursos.png'
-import ingles from './assets/images/bandeira.png'
-import info from './assets/images/info.png'
-import eletro from './assets/images/eletro.png'
-import olho from './assets/images/icon_olho.png'
-import carro from './assets/images/carro.png'
-import anpul from './assets/images/ampulheta.png'
+import logo from './assets/images/logo.png';
+import curso from './assets/images/cursos.png';
+import ingles from './assets/images/bandeira.png';
+import info from './assets/images/info.png';
+import eletro from './assets/images/eletro.png';
+import olho from './assets/images/icon_olho.png';
+import carro from './assets/images/carro.png';
+import anpul from './assets/images/ampulheta.png';
 
 export default function Inicio() {
-
-    
-     return (
-        <div>
-            <header>
-                <div className="cabecalho">
-
-                  <div className="principal">
-                    <img src={logo} className='logo' />
-                    <h1>Instituto Nóssa Senhora de Fátima</h1>
-                    </div>
-                <Link to={'/cadastro'} className='inscricao'>INSCREVA-SE</Link>
-                </div>
-               
-            </header>
-
-            <main>
-
-                <div className="container-image">
-
-
-                    <div className="Container-informacao ">
-                        <h1>SEJAM TODOS BEM VINDOS </h1>
-                        <hr />
-                        
-                        <h3>Esforço que  transforma, e o <br />orgulho permanece.</h3>
-                        <Link to={'/cadastro'} className='inscricao'>GARANTA SUA INSCRIÇÃO</Link>
-                    </div>
-
-                    <div className="imagem2 ">
-                        <img src={curso} />
-
-                    </div>
-
-
-                </div>
-            </main>
-
-            <h2>Cursos Participantes</h2>
-            
-            <div className="carrosel">
-
-
-                    <div className="curso-card">
-          <img src={ingles} alt="Curso de Inglês" className="card-image" />
-          
-            <h3>INGLÊS</h3>
-            <p>Intermediário e avançado</p>
-          
+  return (
+    <div>
+      {/* HEADER */}
+      <header>
+        <div className="cabecalho">
+          <div className="principal">
+            <img src={logo} className="logo" alt="Logo Instituto" />
+            <h1>Instituto Nóssa Senhora de Fátima</h1>
+          </div>
+          <Link to={'/cadastro'} className="inscricao">
+            INSCREVA-SE
+          </Link>
         </div>
+      </header>
 
-             <div className="curso-card">
-          <img src={info} alt="Curso de Informática" className="card-image" />
+      
+      <main>
+        <div className="container-image">
           
-            <h3>INFORMÁTICA</h3>
-            <p>Básico e avançado</p>
-          
-        </div>
-
-        {/* Card 3 - Elétrica */}
-        <div className="curso-card">
-          <img src={eletro} alt="Curso de Elétrica" className="card-image" />
-          
-            <h3>ELÉTRICA</h3>
-            <p>Eletricista Instalador</p>
-          
-        </div>
-
-        {/* Card 4 - Comunicação Visual */}
-        <div className="curso-card">
-          <img src={olho} alt="Curso de Comunicação Visual" className="card-image" />
-          
-            <h3>COMUNICAÇÃO VISUAL</h3>
-            <p>Design intuitivo</p>
-          
-        </div>
-
-        {/* Card 5 - Eletrônica de Carros */}
-        <div className="curso-card">
-          <img src={carro} alt="Curso de Eletrônica Automotiva" className="card-image" />
-          
-            <h3>ELETRÔNICA DE CARROS</h3>
-            <p>Estética automotiva</p>
-          
-        </div>
-
-        {/* Card 6 - Administração */}
-        <div className="curso-card">
-          <img src={anpul} alt="Curso de Administração" className="card-image" />
-          
-            <h3>ADMINISTRAÇÃO</h3>
-            <p>Gestão administrativa</p>
-          
-        </div>
-
-            </div>
-
-
-
-            
-
-          <div className="caixa_andar"> 
-            <h1>PATIO</h1>
-
-          <div className="caixa_texto">
-
-           
-            <ul>
-              <li><strong>Estandes de empresas convidadas</strong></li>
-                <ul>
-                  <li>Casa da Mulher Paulistana</li>
-                  <li>Conexão Bem Maior</li>
-                  <li>Óticas Carol</li>
-                  <li>STB</li>
-                </ul>
-              
-              <h3><strong>Demais estandes</strong></h3>
-                
-                  <li><strong>CEDESP AVE MARIA</strong></li>
-                    <ul>
-                      <li>Metalmecânica – Ajustador e Torneiro Mecânico</li>
-                      <li>Tecnologia de Redes e Manutenção de Computadores</li>
-                    </ul>
-                  <ul>
-                  <li>Troca de Livros</li>
-                  <li>Oficina de Eletromecânica de Autos</li>
-                  <li>Oficina de Auto Elétrica</li>
-                  <li>Oficina Eletrotécnica</li>
-                  <li>Livros do Frei</li>
-                  <li>Produtos da Padaria do Frei – doces e salgados</li>
-                  <li>Pastel e bebidas (embaixo da escada)</li>
-                </ul>
-                
-                </ul>
-            </div>
-          <img src="" alt="" className="atividae_nadar" />
-
-
+          <div className="Container-informacao">
+            <h1>SEJAM TODOS BEM VINDOS</h1>
+            <hr />
+            <h3>
+              Esforço que transforma, e o <br />
+              orgulho permanece.
+            </h3>
+            <Link to={'/cadastro'} className="inscricao">
+              GARANTA SUA INSCRIÇÃO
+            </Link>
           </div>
 
-
-
-
-
-            
-            
-        
+          {/* Imagem principal */}
+          <div className="imagem2">
+            <img src={curso} alt="Cursos" />
+          </div>
         </div>
-    )
+      </main>
+
+      {/* CURSOS */}
+      <section>
+        <h2>Cursos Participantes</h2>
+        <div className="carrosel">
+          {/* Card 1 - Inglês */}
+          <div className="curso-card">
+            <img src={ingles} alt="Curso de Inglês" className="card-image" />
+            <h3>INGLÊS</h3>
+            <p>Intermediário e avançado</p>
+          </div>
+
+          {/* Card 2 - Informática */}
+          <div className="curso-card">
+            <img src={info} alt="Curso de Informática" className="card-image" />
+            <h3>INFORMÁTICA</h3>
+            <p>Básico e avançado</p>
+          </div>
+
+          {/* Card 3 - Elétrica */}
+          <div className="curso-card">
+            <img src={eletro} alt="Curso de Elétrica" className="card-image" />
+            <h3>ELÉTRICA</h3>
+            <p>Eletricista Instalador</p>
+          </div>
+
+          {/* Card 4 - Comunicação Visual */}
+          <div className="curso-card">
+            <img src={olho} alt="Curso de Comunicação Visual" className="card-image" />
+            <h3>COMUNICAÇÃO VISUAL</h3>
+            <p>Design intuitivo</p>
+          </div>
+
+          {/* Card 5 - Eletrônica de Carros */}
+          <div className="curso-card">
+            <img src={carro} alt="Curso de Eletrônica Automotiva" className="card-image" />
+            <h3>ELETRÔNICA DE CARROS</h3>
+            <p>Estética automotiva</p>
+          </div>
+
+          {/* Card 6 - Administração */}
+          <div className="curso-card">
+            <img src={anpul} alt="Curso de Administração" className="card-image" />
+            <h3>ADMINISTRAÇÃO</h3>
+            <p>Gestão administrativa</p>
+          </div>
+        </div>
+      </section>
+
+      
+      <section className="caixa_andar">
+        <h1>PÁTIO</h1>
+        <div className="informacao_andar">
+          <div className="caixa_texto">
+            <h3>
+              <strong>Estandes de empresas convidadas</strong>
+            </h3>
+            <ul>
+              <li>Casa da Mulher Paulistana</li>
+              <li>Conexão Bem Maior</li>
+              <li>Óticas Carol</li>
+              <li>STB</li>
+            </ul>
+
+            <h3>
+              <strong>Demais estandes</strong>
+            </h3>
+            <ul>
+              <li>CEDESP AVE MARIA</li>
+              <li>Metalmecânica – Ajustador e Torneiro Mecânico</li>
+              <li>Tecnologia de Redes e Manutenção de Computadores</li>
+            </ul>
+            <ul>
+              <li>Troca de Livros</li>
+              <li>Oficina de Eletromecânica de Autos</li>
+              <li>Oficina de Auto Elétrica</li>
+              <li>Oficina Eletrotécnica</li>
+              <li>Livros do Frei</li>
+              <li>Produtos da Padaria do Frei – doces e salgados</li>
+              <li>Pastel e bebidas (embaixo da escada)</li>
+            </ul>
+
+            <h3>Salas</h3>
+            <ul>
+              <li>
+                <strong>SALA 4:</strong> Óticas Carol
+              </li>
+              <li>
+                <strong>SALA 5:</strong> Caixa
+              </li>
+              <li>
+                <strong>SALA 6:</strong> Inglês
+              </li>
+            </ul>
+          </div>
+
+          <img src="" alt="" className="atividae_nadar" />
+        </div>
+      </section>
+    </div>
+  );
 }
